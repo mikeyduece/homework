db895e63ef71d792cdd4454b853f532d15447449
Mike Heft <mikeheft@gmail.com>
2017-05-02 09:09:52 -0600

    initial commit

---
+++ pseudocode.rb
@@ -0,0 +1,19 @@
+ready_to_quit = false
+puts "HELLO, THIS IS A GROCERY STORE!"
+
+input = gets
+until ready_to_quit
+  if the input is empty
+    puts "HELLO?!"
+  all lowercase letter in the input
+    puts "I AM HAVING A HARD TIME HEARING YOU."
+  set counter to zero
+    increse count based on input
+  input goodbye
+  counter equals 1
+    puts "ANYTHING ELSE I CAN HELP WITH?"
+  counter equals 2
+    puts "THANK YOU FOR CALLING!"
+end
+
+puts "THANK YOU FOR CALLING!"
