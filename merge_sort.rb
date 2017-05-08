9b6f0bcaadd20dbde61d672bcc87749d7d3a11b8
Mike Heft <mikeheft@gmail.com>
2017-05-07 21:01:44 -0600

    Add files via upload
    not finished. It's on the tip of my tongue, like I can see the solution but unable to see the disconnect to get it there.

---
+++ merge_sort.rb
@@ -0,0 +1,23 @@
+require 'pry'
+class MergeSort
+  def sort(input)
+    # seperate array into odd and even array
+    input_even = []
+    input_odd = []
+    input.map.with_index {|num, i| i.even? ? input_even << input[i] : input_odd << input[i]}
+    # perform insertion sort on both arrays
+    input_even.map.with_index {|num, i| (i-1) < i ? input_even.insert(num) : nil}
+    input_odd.map.with_index {|num, i| (i-1) < i ? input_odd.insert(num) : nil}
+    # combine both arrays
+    sorted = []
+    
+    binding.pry
+
+  end
+end
+
+
+sorter = MergeSort.new
+#=> #<MergeSort:0x007f81a19e94e8>
+p sorter.sort(["d", "b", "a", "c"])
+#=> ["a", "b", "c", "d"]
